adacea310f55e9617f2ce8f805249184ecef158c
AuzarKhant <auzarkhant.2@gmail.com>
2024-04-29 12:15:56 +0700

    test15.py

--- test15.py
+++ test15.py
@@ -3,4 +3,9 @@ fav_bike = {
     'brand' : ' Yamaha',
     'generation' : 2022
 }
-print(fav_bike['brand']) 
+# # print(fav_bike['brand']) 
+# fav_bike['brand'] = 'Honda'
+# print(fav_bike['brand'])
+
+fav_bike["newest_model"] = 2024
+print(fav_bike)
