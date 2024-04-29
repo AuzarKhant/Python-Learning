c3137771487e53c3218079f8932e16f8f834a4ff
AuzarKhant <auzarkhant.2@gmail.com>
2024-04-29 11:55:42 +0700

    test14.py

--- test14.py
+++ test14.py
@@ -7,5 +7,10 @@ print(type(mylist))
 #print(mylist[2:])
 #print(mylist[:5])
 
-if 'boots' in mylist:
-    print("yes it is!!")
+# if 'boots' in mylist:
+#     print("yes it is!!")
+# mylist.insert(1, "jacket")
+# print(mylist)
+
+mylist[2] = 'gauntlet'
+print(mylist)
