22c91ae88306fb2513756b93c110f403493c4744
AuzarKhant <auzarkhant.2@gmail.com>
2024-04-29 11:34:44 +0700

    test14.py

--- test14.py
+++ test14.py
@@ -1,3 +1,8 @@
-mylist = ['tyres', 'gloves', 'helmelt', 'boots', 'keys']
+mylist = ['tyres', 'gloves', 'helmelt', 'boots', 'keys', 'cardo']     
 print(mylist)
 print(type(mylist))
+
+print(mylist[0])
+print(mylist[1:5])
+print(mylist[2:])
+print(mylist[:5])
