5b9d8905e47a60a1220e2c48ba8693a30419955e
AuzarKhant <auzarkhant.2@gmail.com>
2024-04-30 12:05:53 +0700

    def, import

---
+++ test17.py
@@ -0,0 +1,16 @@
+def add(x,y):
+    answer = x + y
+    print(answer)
+
+def sub(x,y):
+    answer = x - y
+    print(answer)
+
+def div(x,y):
+    answer = x / y
+    print(answer)
+
+def mul(x,y):
+    answer = x * y
+    print(answer)
+    
---
+++ test18.py
@@ -0,0 +1,5 @@
+from test17 import * 
+add(6,8)
+sub(8,4)
+mul(21,6)
+div(12,2)
