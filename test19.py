fc511ec7e70afc2fa1d612686accd98382d1bc88
AuzarKhant <auzarkhant.2@gmail.com>
2024-04-30 12:23:52 +0700

    test19+20

---
+++ test19.py
@@ -0,0 +1,4 @@
+def user_info(name, mail, address):
+    print("Your name is ", name)
+    print("Your mail is ", mail)
+    print("Your address is ", address)
---
+++ test20.py
@@ -0,0 +1,3 @@
+from test19 import user_info
+
+user_info(name="AK47", mail="ak47@gmail.com", address="uranus")
