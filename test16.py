4b06401d7cee0ce01cfcedd591f0d07ce99c7759
AuzarKhant <auzarkhant.2@gmail.com>
2024-04-30 11:55:20 +0700

    test16.py

---
+++ test16.py
@@ -0,0 +1,28 @@
+user_info = {
+    'Name': 'Auzar',
+    'fav_bikes' : {
+        'bike1' : 'Yamaha R1',
+        'bike2' : 'Honda grom',
+        'bike3' : 'Kawasaki zx6r',
+    },
+    'Topspeed' : {
+        'bike1' : '300kmph+',
+        'bike2' : '100kmph',
+        'bike3' : '250kmph+',
+    }
+}
+
+    
+print(user_info['Name'])
+print(user_info['fav_bikes']['bike3'])
+print(user_info['Topspeed']['bike3'])
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
